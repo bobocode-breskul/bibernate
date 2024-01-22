5cd9386f0eeee666a5bdac27c311fa6f54c2c72c
RomanPryshchepa <roman.pryshchepa@gmail.com>
2024-01-22 20:32:35 +0100

    Moved utility methods to utility class

--- src/main/java/com/breskul/bibernate/persistence/GenericDao.java
+++ src/main/java/com/breskul/bibernate/persistence/GenericDao.java
@@ -1,19 +1,16 @@
 package com.breskul.bibernate.persistence;
 
-import com.breskul.bibernate.annotation.Column;
-import com.breskul.bibernate.annotation.Entity;
-import com.breskul.bibernate.annotation.Id;
-import com.breskul.bibernate.annotation.Table;
-import com.breskul.bibernate.exception.EntityParseException;
-import com.breskul.bibernate.exception.EntityQueryException;
+import static com.breskul.bibernate.util.EntityUtil.composeSelectBlockFromColumns;
+import static com.breskul.bibernate.util.EntityUtil.doQuery;
+import static com.breskul.bibernate.util.EntityUtil.findEntityIdField;
+import static com.breskul.bibernate.util.EntityUtil.getClassColumnFields;
+import static com.breskul.bibernate.util.EntityUtil.getEntityTableName;
+import static com.breskul.bibernate.util.EntityUtil.getIdColumnName;
+import static com.breskul.bibernate.util.EntityUtil.validateIsEntity;
 
 import javax.sql.DataSource;
 import java.lang.reflect.Field;
-import java.sql.*;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class GenericDao {
 
@@ -35,74 +32,6 @@ public class GenericDao {
         String sql = SELECT_BY_ID_QUERY.formatted(composeSelectBlockFromColumns(columnFields),
                 tableName, getIdColumnName(idField));
 
-        return doQuery(sql, id, new EntityMapper<T>(cls, columnFields));
-    }
-
-    private <T> T doQuery(String sql, Object id, EntityMapper<T> entityMapper) {
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setObject(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return entityMapper.mapResult(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new EntityQueryException("Could not read entity data from database for entity [%s] with id [%s]"
-                    .formatted(entityMapper.getEntityCls(), id), e);
-        }
-        return null;
-    }
-
-    private <T> void validateIsEntity(Class<T> cls) {
-        if (!cls.isAnnotationPresent(Entity.class)) {
-            throw new EntityParseException("Clas should be marked with 'Entity' annotation");
-        }
-    }
-
-    private String getEntityTableName(Class<?> cls) {
-        return Optional.ofNullable(cls.getAnnotation(Table.class))
-                .map(Table::name)
-                .orElseThrow(() -> new EntityParseException("Class should be marked with 'Table' annotation and it should not be empty"));
-    }
-
-    private List<Field> getClassColumnFields(Class<?> cls) {
-        return Arrays.stream(cls.getDeclaredFields())
-                .filter(field -> field.isAnnotationPresent(Column.class) || field.isAnnotationPresent(Id.class))
-                .toList();
-    }
-
-    private Field findEntityIdField(List<Field> fields) {
-        List<Field> idFields = fields.stream()
-                .filter(field -> field.isAnnotationPresent(Id.class))
-                .toList();
-        if (idFields.isEmpty()) {
-            throw new EntityParseException("Entity should define ID column marked with 'Id' annotation.");
-        }
-        if (idFields.size() > 1) {
-            throw new EntityParseException("Only one field should be marked with 'Id' annotation.");
-        }
-        return idFields.get(0);
-    }
-
-    private String composeSelectBlockFromColumns(List<Field> columnNames) {
-        return columnNames.stream()
-                .map(this::getFieldColumnName)
-                .collect(Collectors.joining(", "));
-    }
-
-    private String getFieldColumnName(Field field) {
-        if (field.isAnnotationPresent(Column.class)) {
-            return field.getAnnotation(Column.class).name();
-        } else if (field.isAnnotationPresent(Id.class)) {
-            return getIdColumnName(field);
-        }
-        throw new IllegalArgumentException("Field should have annotation of type [Column.class, Id.class]");
-    }
-
-    private String getIdColumnName(Field idField) {
-        if (idField.isAnnotationPresent(Column.class)) {
-            return idField.getAnnotation(Column.class).name();
-        }
-        return idField.getName();
+        return doQuery(sql, id, new EntityMapper<>(cls, columnFields), dataSource);
     }
 }
---
+++ src/main/java/com/breskul/bibernate/util/EntityUtil.java
@@ -0,0 +1,97 @@
+package com.breskul.bibernate.util;
+
+import com.breskul.bibernate.annotation.Column;
+import com.breskul.bibernate.annotation.Entity;
+import com.breskul.bibernate.annotation.Id;
+import com.breskul.bibernate.annotation.Table;
+import com.breskul.bibernate.exception.EntityParseException;
+import com.breskul.bibernate.exception.EntityQueryException;
+import com.breskul.bibernate.persistence.EntityMapper;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.sql.DataSource;
+
+public class EntityUtil {
+
+  public static <T> T doQuery(String sql, Object id, EntityMapper<T> entityMapper, DataSource dataSource) {
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setObject(1, id);
+      ResultSet resultSet = statement.executeQuery();
+      if (resultSet.next()) {
+        return entityMapper.mapResult(resultSet);
+      }
+    } catch (SQLException e) {
+      throw new EntityQueryException(
+          "Could not read entity data from database for entity [%s] with id [%s]"
+              .formatted(entityMapper.getEntityCls(), id), e);
+    }
+    return null;
+  }
+
+  public static <T> void validateIsEntity(Class<T> cls) {
+    if (!cls.isAnnotationPresent(Entity.class)) {
+      throw new EntityParseException("Class should be marked with 'Entity' annotation");
+    }
+  }
+
+  public static String getEntityTableName(Class<?> cls) {
+    return Optional.ofNullable(cls.getAnnotation(Table.class))
+        .map(Table::name)
+        .orElseThrow(() -> new EntityParseException(
+            "Class should be marked with 'Table' annotation and it should not be empty"));
+  }
+
+  public static List<Field> getClassColumnFields(Class<?> cls) {
+    return Arrays.stream(cls.getDeclaredFields())
+        .filter(field -> field.isAnnotationPresent(Column.class) || field.isAnnotationPresent(
+            Id.class))
+        .toList();
+  }
+
+  public static Field findEntityIdField(List<Field> fields) {
+    List<Field> idFields = fields.stream()
+        .filter(field -> field.isAnnotationPresent(Id.class))
+        .toList();
+    if (idFields.isEmpty()) {
+      throw new EntityParseException("Entity should define ID column marked with 'Id' annotation.");
+    }
+    if (idFields.size() > 1) {
+      throw new EntityParseException("Only one field should be marked with 'Id' annotation.");
+    }
+    return idFields.get(0);
+  }
+
+  public static String composeSelectBlockFromColumns(List<Field> columnNames) {
+    return columnNames.stream()
+        .map(EntityUtil::getFieldColumnName)
+        .collect(Collectors.joining(", "));
+  }
+
+  public static String getIdColumnName(Field idField) {
+    if (idField.isAnnotationPresent(Column.class)) {
+      return idField.getAnnotation(Column.class).name();
+    }
+    return idField.getName();
+  }
+
+  private static String getFieldColumnName(Field field) {
+    if (field.isAnnotationPresent(Column.class)) {
+      return field.getAnnotation(Column.class).name();
+    } else if (field.isAnnotationPresent(Id.class)) {
+      return getIdColumnName(field);
+    }
+    throw new IllegalArgumentException(
+        "Field should have annotation of type [Column.class, Id.class]");
+  }
+
+  private EntityUtil() {
+  }
+}
