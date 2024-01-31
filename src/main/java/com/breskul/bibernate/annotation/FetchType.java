dff5abbdbb13527c417a3dd02a06d61df119fc53
Vitalii Katkov <katkovvitaliy@gmail.com>
2024-01-31 20:06:49 +0200

    Corrected fetch type enum documentation

--- src/main/java/com/breskul/bibernate/annotation/FetchType.java
+++ src/main/java/com/breskul/bibernate/annotation/FetchType.java
@@ -3,10 +3,12 @@ package com.breskul.bibernate.annotation;
 
 /**
  * Enumeration representing the different fetch types for entity associations.
- *
+ * <br/>
  * The FetchType enumeration provides two fetch types:
+ * <br/>
  * - EAGER: This fetch type specifies that the associated entity or collection should be eagerly
  * fetched when the owning entity is fetched.
+ * <br/>
  * - LAZY: This fetch type specifies that the associated entity or collection should be lazily
  * fetched when the owning entity is fetched.
  */
