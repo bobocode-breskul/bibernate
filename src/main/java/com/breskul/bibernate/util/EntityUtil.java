060c8ece5204db802ee4b4a1ee6b14151cd93366
Vitalii Katkov <katkovvitaliy@gmail.com>
2024-02-02 21:55:46 +0200

    EntityUtil clean-up

--- src/main/java/com/breskul/bibernate/util/EntityUtil.java
+++ src/main/java/com/breskul/bibernate/util/EntityUtil.java
@@ -1,29 +1,20 @@
 package com.breskul.bibernate.util;
 
+import static com.breskul.bibernate.util.ReflectionUtil.readFieldValue;
+
 import com.breskul.bibernate.annotation.Column;
 import com.breskul.bibernate.annotation.Entity;
 import com.breskul.bibernate.annotation.Id;
 import com.breskul.bibernate.annotation.JoinColumn;
 import com.breskul.bibernate.annotation.ManyToMany;
-import com.breskul.bibernate.annotation.ManyToOne;
 import com.breskul.bibernate.annotation.OneToMany;
-import com.breskul.bibernate.annotation.OneToOne;
 import com.breskul.bibernate.annotation.Table;
-import com.breskul.bibernate.collection.LazyList;
-import com.breskul.bibernate.collection.LazySet;
 import com.breskul.bibernate.exception.EntityParseException;
-import com.breskul.bibernate.persistence.Test;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 
@@ -45,38 +36,37 @@ public class EntityUtil {
   }
 
   /**
-   * Validates if the given column exists in the entity class.
-   * Throws an exception if the column does not exist.
+   * Validates if the given column exists in the entity class. Throws an exception if the column
+   * does not exist.
    *
    * @param cls        - The entity class
    * @param columnName - The column name to validate
-   *
    * @throws IllegalArgumentException if the column does not exist in the entity class
    */
   public static void validateColumnName(Class<?> cls, String columnName) {
     validateIsEntity(cls);
     var column = getClassColumnFields(cls).stream()
-      .filter(field -> resolveColumnName(field).equals(columnName))
-      .findFirst();
+        .filter(field -> resolveColumnName(field).equals(columnName))
+        .findFirst();
     if (column.isEmpty()) {
       throw new IllegalArgumentException("Entity [%s] does not have a column: [%s]."
-        .formatted(cls, columnName));
+          .formatted(cls, columnName));
     }
   }
 
 
   /**
    * Retrieves the table name for the given entity class. If the class is annotated with the
-   * {@link Table} annotation, the table name from the annotation is returned. Otherwise, the
-   * class name is returned.
+   * {@link Table} annotation, the table name from the annotation is returned. Otherwise, the class
+   * name is returned.
    *
    * @param cls - The entity class
    * @return The table name for the entity class
    */
   public static String getEntityTableName(Class<?> cls) {
     return Optional.ofNullable(cls.getAnnotation(Table.class))
-      .map(Table::name)
-      .orElseGet(cls::getSimpleName);
+        .map(Table::name)
+        .orElseGet(cls::getSimpleName);
   }
 
 
@@ -88,8 +78,8 @@ public class EntityUtil {
    */
   public static List<Field> getClassColumnFields(Class<?> cls) {
     return Arrays.stream(cls.getDeclaredFields())
-      .filter(field -> !isCollectionEntityField(field))
-      .toList();
+        .filter(field -> !isCollectionEntityField(field))
+        .toList();
   }
 
 
@@ -101,7 +91,7 @@ public class EntityUtil {
    */
   public static boolean isCollectionEntityField(Field field) {
     return field.isAnnotationPresent(OneToMany.class)
-      || field.isAnnotationPresent(ManyToMany.class);
+        || field.isAnnotationPresent(ManyToMany.class);
   }
 
 
@@ -113,7 +103,7 @@ public class EntityUtil {
    */
   public static List<Field> getClassEntityFields(Class<?> cls) {
     return Arrays.stream(cls.getDeclaredFields())
-      .toList();
+        .toList();
   }
 
   /**
@@ -122,7 +112,7 @@ public class EntityUtil {
    * @param fields - The list of fields to search
    * @return The entity ID field
    * @throws EntityParseException if the entity does not define an ID column or if multiple fields
-   *         are marked with the 'Id' annotation
+   *                              are marked with the 'Id' annotation
    */
   public static Field findEntityIdField(List<Field> fields) {
     List<Field> idFields = fields.stream()
@@ -143,7 +133,7 @@ public class EntityUtil {
    * @param cls - The entity class
    * @return The entity ID field
    * @throws EntityParseException if the entity does not define an ID column or if multiple fields
-   *         are marked with the 'Id' annotation
+   *                              are marked with the 'Id' annotation
    */
   public static Field findEntityIdField(Class<?> cls) {
     validateIsEntity(cls);
@@ -185,17 +175,17 @@ public class EntityUtil {
   /**
    * Retrieves the join column name for joined entity.
    *
-   * @param entityType    - The entity type
-   * @param joinedEntity  - The joined entity
+   * @param entityType   - The entity type
+   * @param joinedEntity - The joined entity
    * @return The join column name
    * @throws IllegalStateException if the related entity field cannot be found in the entity type
    */
   public static String getJoinColumnName(Class<?> entityType, Class<?> joinedEntity) {
     var joinField = Arrays.stream(entityType.getDeclaredFields())
-      .filter(field -> field.getType().equals(joinedEntity))
-      .findFirst()
-      .orElseThrow(() -> new IllegalStateException("Can't find related entity [%s] field in [%s]."
-        .formatted(joinedEntity, entityType)));
+        .filter(field -> field.getType().equals(joinedEntity))
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("Can't find related entity [%s] field in [%s]."
+            .formatted(joinedEntity, entityType)));
 
     return resolveColumnName(joinField);
   }
@@ -207,7 +197,7 @@ public class EntityUtil {
    * @param entity - The entity object
    * @return The ID value of the entity object
    * @throws EntityParseException if the entity does not define an ID column or if multiple fields
-   *         are marked with the 'Id' annotation
+   *                              are marked with the 'Id' annotation
    */
   public static Object getEntityId(Object entity) {
     var idField = findEntityIdField(List.of(entity.getClass().getDeclaredFields()));
@@ -237,68 +227,6 @@ public class EntityUtil {
     var typeArguments = parameterizedType.getActualTypeArguments();
     var actualTypeArgument = typeArguments[0];
     return (Class<?>) actualTypeArgument;
-  }
-
-
-  /**
-   * Retrieves an instance of the Collection class based on the given Field representing a collection.
-   *
-   * @param collectionField - The Field representing the collection
-   * @return An instance of the Collection class
-   * @throws IllegalArgumentException if the collection type is unsupported
-   */
-  public static Collection<Object> getCollectionInstance(Field collectionField) {
-    return getCollectionInstance(collectionField, Collections.emptyList());
-  }
-
-  // todo: docs
-  public static Collection<Object> getCollectionInstance(Field collectionField, Collection<?> source) {
-    var collectionClass = collectionField.getType();
-
-    if (collectionClass.isAssignableFrom(List.class)) {
-      return new ArrayList<>(source);
-    }
-
-    if (collectionClass.isAssignableFrom(Set.class)) {
-      return new HashSet<>(source);
-    }
-
-    throw new IllegalArgumentException("Unsupported collection: " + collectionClass); // change exception and more clear msg?
-  }
-
-  // todo: docs
-  public static Collection<Object> getLazyCollectionInstance(Field collectionField, Supplier<Collection<?>> delegateSupplier) {
-    var collectionClass = collectionField.getType();
-
-    if (collectionClass.isAssignableFrom(List.class)) {
-      return new LazyList<>(delegateSupplier);
-    }
-
-    if (collectionClass.isAssignableFrom(Set.class)) {
-      return new LazySet<>(delegateSupplier);
-    }
-
-    throw new IllegalArgumentException("Unsupported collection: " + collectionClass); // change exception and more clear msg?
-  }
-
-
-  /**
-   * Reads the value of a field on the given entity object.
-   *
-   * @param entity - The entity object
-   * @param idField - The field to read the value from
-   * @return The value of the field
-   * @throws EntityParseException if failed to access the field due to illegal access
-   */
-  public static Object readFieldValue(Object entity, Field idField) {
-    try {
-      idField.setAccessible(true);
-      return idField.get(entity);
-    } catch (IllegalAccessException e) {
-      throw new EntityParseException(
-          "Failed to access field '" + idField.getName() + "' of entity type '" + entity.getClass()
-              .getName() + "': Illegal access");
-    }
   }
 
   private EntityUtil() {
