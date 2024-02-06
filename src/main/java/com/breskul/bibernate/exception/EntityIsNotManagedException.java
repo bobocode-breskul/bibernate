3f40dec83e7a3ce3f1a2d916e7072fc68b8e4fed
Ruslan Hladchenko <rugladchenko@gmail.com>
2024-02-06 21:15:51 +0200

    Add custom exception if entity is not in a persistence context

---
+++ src/main/java/com/breskul/bibernate/exception/EntityIsNotManagedException.java
@@ -0,0 +1,13 @@
+package com.breskul.bibernate.exception;
+
+/**
+ * Thrown to indicate that an operation was attempted on an entity that is not managed. This
+ * typically occurs when an entity is expected to be in a managed state for a particular operation
+ * (e.g., update, delete), but the entity is not present in the current persistence context.
+ */
+public class EntityIsNotManagedException extends RuntimeException {
+
+  public EntityIsNotManagedException(String message) {
+    super(message);
+  }
+}
