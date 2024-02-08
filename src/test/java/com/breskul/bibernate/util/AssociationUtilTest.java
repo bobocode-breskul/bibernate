a917bdcf1ed07d2e3804302b05336a6477f7c78a
Vitalii Katkov <katkovvitaliy@gmail.com>
2024-02-08 22:35:19 +0200

    Half covered AssociationUtil getLazyCollectionInstance method with unit tests

--- src/test/java/com/breskul/bibernate/util/AssociationUtilTest.java
+++ src/test/java/com/breskul/bibernate/util/AssociationUtilTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.breskul.bibernate.exception.AssociationException;
+import com.breskul.bibernate.proxy.collection.LazyList;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,6 +22,7 @@ class AssociationUtilTest {
   private static final String SET_FIELD_NAME = "setField";
   private static final String INTEGER_FIELD_NAME = "integerField";
   private static final List<String> SAMPLE_DATA_LIST = List.of("sample_data_1", "sample_data_2");
+  private static final String LAZY_DELEGATE_FIELD = "delegate";
 
   @Test
   @DisplayName("When field type is 'List' then created collection is empty 'ArrayList'")
@@ -87,7 +89,7 @@ class AssociationUtilTest {
 
   @Test
   @DisplayName("When field type is 'List' and source collection has data then created "
-      + "collection has the same data")
+      + "'ArrayList' collection has the same data")
   @Order(5)
   @SneakyThrows
   void whenGetCollectionInstanceWithValidSourceList_thenResultCollectionHasCorrectData() {
@@ -118,7 +120,7 @@ class AssociationUtilTest {
 
   @Test
   @DisplayName("When field type is 'Set' and source collection has data then created "
-      + "collection has the same data")
+      + "'HashSet' collection has the same data")
   @Order(7)
   @SneakyThrows
   void whenGetCollectionInstanceWithValidSourceSet_thenResultCollectionHasCorrectData() {
@@ -147,6 +149,58 @@ class AssociationUtilTest {
         .hasMessage("Entity association collection is not supported. Collection: [%s]".formatted(
             inputIntegerField.getType()));
   }
+
+  @Test
+  @DisplayName("When field type is 'List' and delegate collection has data then "
+      + "created collection is 'LazyList'")
+  @Order(9)
+  @SneakyThrows
+  void whenGetLazyCollectionInstanceWithValidDelegateList_thenResultCollectionIsLazyList() {
+    // data
+    Field inputListField = SampleEntity.class.getDeclaredField(LIST_FIELD_NAME);
+    // when
+    Collection<Object> resultCollection = AssociationUtil.getLazyCollectionInstance(inputListField,
+        () -> SAMPLE_DATA_LIST);
+    // verify
+    assertThat(resultCollection).isInstanceOf(LazyList.class);
+  }
+
+  @Test
+  @DisplayName("When field type is 'List' and delegate collection has data then "
+      + "created 'LazyList' is not initialized")
+  @Order(10)
+  @SneakyThrows
+  void whenGetLazyCollectionInstanceWithValidDelegateList_thenResultLazyListIsNotInitialized() {
+    // data
+    Field inputListField = SampleEntity.class.getDeclaredField(LIST_FIELD_NAME);
+    // when
+    Collection<Object> resultCollection = AssociationUtil.getLazyCollectionInstance(inputListField,
+        () -> SAMPLE_DATA_LIST);
+    // verify
+    Field lazyDelegateField = LazyList.class.getDeclaredField(LAZY_DELEGATE_FIELD);
+    lazyDelegateField.setAccessible(true);
+    assertThat(lazyDelegateField.get(resultCollection)).isNull();
+  }
+
+  @Test
+  @DisplayName("When field type is 'List' and delegate collection has data then "
+      + "'LazyList' collection has the same data")
+  @Order(11)
+  @SneakyThrows
+  void whenGetLazyCollectionInstanceWithValidDelegateList_thenResultCollectionHasCorrectData() {
+    // data
+    Field inputListField = SampleEntity.class.getDeclaredField(LIST_FIELD_NAME);
+    // when
+    Collection<Object> resultCollection = AssociationUtil.getLazyCollectionInstance(inputListField,
+        () -> SAMPLE_DATA_LIST);
+    // verify
+    assertThat(resultCollection).containsExactlyElementsOf(SAMPLE_DATA_LIST);
+  }
+
+  // todo: create test, created collection is LazySet
+  // todo: create test, created 'LazySet' is not initialized
+  // todo: create test, created 'LazySet' has same data
+  // todo: create test, throw exception for invalid field
 
   /**
    * Class with different fields for testing purpose
