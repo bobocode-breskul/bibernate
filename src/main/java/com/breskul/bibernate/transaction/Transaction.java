90b8e9f2ea15edfa56ab6e921e8e5ce3ff8d3a7a
Serhii Barabash <sbarabash@marqeta.com>
2024-02-11 12:54:37 +0200

    36 resolve comments

--- src/main/java/com/breskul/bibernate/transaction/Transaction.java
+++ src/main/java/com/breskul/bibernate/transaction/Transaction.java
@@ -36,14 +36,11 @@ public class Transaction {
    */
   public void begin() {
     try {
+      validateIfSessionOpen();
       status = TransactionStatus.ACTIVE;
-      validateIfSessionOpenAndTransactionActive();
       connection.setAutoCommit(false);
       log.trace("Begin transaction");
     } catch (SQLException e) {
-      if (status == TransactionStatus.ACTIVE) {
-        status = TransactionStatus.NOT_ACTIVE;
-      }
       setAutoCommitTrue();
       throw new BibernateException("Error occurred during opening transaction", e);
     }
@@ -95,10 +92,14 @@ public class Transaction {
     return status;
   }
 
-  private void validateIfSessionOpenAndTransactionActive() throws SQLException {
+  private void validateIfSessionOpen() throws SQLException {
     if (!session.isOpen()) {
       throw new IllegalStateException("Cannot operate Transaction on closed session");
     }
+  }
+
+  private void validateIfSessionOpenAndTransactionActive() throws SQLException {
+    validateIfSessionOpen();
     if (status != TransactionStatus.ACTIVE) {
       throw new IllegalStateException("Transaction is not active");
     }
