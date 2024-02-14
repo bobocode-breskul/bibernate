ab0a9eddd8d3b75b36dcf198262d433a195b9d9c
oleg.iakovenko <iakovenkoua@gmail.com>
2024-02-14 16:36:29 +0200

    some fix

--- src/main/java/com/breskul/bibernate/persistence/Persistence.java
+++ src/main/java/com/breskul/bibernate/persistence/Persistence.java
@@ -1,11 +1,9 @@
 package com.breskul.bibernate.persistence;
 
 import com.breskul.bibernate.config.PropertiesConfiguration;
-import com.breskul.bibernate.exception.BibernateException;
 import com.breskul.bibernate.persistence.datasource.BibernateDataSource;
 import com.breskul.bibernate.persistence.datasource.DataSourceProperties;
 import com.breskul.bibernate.persistence.datasource.connectionpools.CentralConnectionPoolFactory;
-import com.breskul.bibernate.persistence.datasource.connectionpools.ConnectionPoolFactory;
 import com.breskul.bibernate.persistence.datasource.propertyreader.ApplicationPropertiesReader;
 
 /**
@@ -21,21 +19,23 @@ import com.breskul.bibernate.persistence.datasource.propertyreader.ApplicationPr
  * </pre>
  */
 public class Persistence {
-
-
   /**
-   * Creates a Hibernate SessionFactory based on configuration properties.
-   *
-   * @return The created Hibernate SessionFactory.
-   * @throws BibernateException If there is an issue with the DataSource or loading the JDBC driver class.
-   * @see BibernateDataSource
-   * @see SessionFactory
-   * @see PropertiesConfiguration
+   * Creates a {@link SessionFactory} instance for managing database sessions.
+   * This method leverages the application's properties to configure and initialize
+   * the data source, and subsequently, the session factory.
+   * <p>
+   * The process involves reading the {@link DataSourceProperties} using the
+   * {@link ApplicationPropertiesReader}, determining the appropriate connection pool
+   * factory based on the type of data source specified in the properties, and then
+   * creating a data source instance. Finally, a new {@link SessionFactory} is instantiated
+   * with the created data source.
+   * </p>
+   * @return A newly created {@link SessionFactory} instance ready for use in creating
+   *         and managing database sessions.
    */
   public static SessionFactory createSessionFactory() {
     DataSourceProperties dataSourceProperties = ApplicationPropertiesReader.getInstance().readProperty();
-//    BibernateDataSource dataSource = new BibernateDataSource(dataSourceProperties);
-    ConnectionPoolFactory factory = CentralConnectionPoolFactory.getConnectionPoolFactory(dataSourceProperties.type());
+    var factory = CentralConnectionPoolFactory.getConnectionPoolFactory(dataSourceProperties.type());
     var dataSource = factory.createDataSource(dataSourceProperties);
     return new SessionFactory(dataSource);
   }
--- src/main/java/com/breskul/bibernate/persistence/datasource/connectionpools/CentralConnectionPoolFactory.java
+++ src/main/java/com/breskul/bibernate/persistence/datasource/connectionpools/CentralConnectionPoolFactory.java
@@ -1,5 +1,7 @@
 package com.breskul.bibernate.persistence.datasource.connectionpools;
 
+import com.breskul.bibernate.persistence.datasource.BibernateDataSource;
+
 /**
  * CentralConnectionPoolFactory serves as a factory class for creating instances
  * of {@link ConnectionPoolFactory} based on a given type. It supports creating
@@ -9,28 +11,29 @@ package com.breskul.bibernate.persistence.datasource.connectionpools;
 public class CentralConnectionPoolFactory {
 
   /**
-   * Returns an instance of {@link ConnectionPoolFactory} based on the specified type.
-   * This method facilitates the dynamic selection of connection pool implementations
-   * at runtime, allowing clients to choose between different connection pooling strategies.
+   * Retrieves an instance of {@link ConnectionPoolFactory} based on the specified type.
+   * This method allows for the dynamic selection of connection pool implementations
+   * at runtime, offering flexibility in choosing the underlying connection pooling mechanism.
    * <p>
-   * Supported types include:
+   * Supported types and their corresponding returned instances include:
    * <ul>
-   *   <li>"Apache" - Returns an instance of {@link ApacheDBCP}.</li>
-   *   <li>"HikariCP" - Returns an instance of {@link HikariCP}.</li>
-   *   <li>"c3p0" - Returns an instance of {@link C3P0}.</li>
+   *   <li>"Apache" - Returns an instance of Apache DBCP connection pool.</li>
+   *   <li>"HikariCP" - Returns an instance of HikariCP connection pool.</li>
+   *   <li>"c3p0" - Returns an instance of c3p0 connection pool.</li>
    * </ul>
-   * If an unsupported type is provided, an {@link IllegalArgumentException} is thrown.
+   * For any other type, this method defaults to returning an instance of
+   * BibernateDataSource, providing a fallback option for database connectivity.
    * </p>
-   * @param type The type of connection pool factory to create. This is a case-sensitive string.
-   * @return An instance of {@link ConnectionPoolFactory} corresponding to the specified type.
-   * @throws IllegalArgumentException If the specified type is not supported.
+   * @param type The type of connection pool factory to create, specified as a case-sensitive string.
+   * @return An instance of {@link ConnectionPoolFactory} corresponding to the specified type,
+   *         or a BibernateDataSource as the default option.
    */
   public static ConnectionPoolFactory getConnectionPoolFactory(String type) {
     return switch (type) {
       case "Apache" -> new ApacheDBCP();
       case "HikariCP" -> new HikariCP();
       case "c3p0" -> new C3P0();
-      default -> throw new IllegalArgumentException("Invalid connection pool type: " + type);
+      default -> new BibernateDataSource();
     };
   }
 }
