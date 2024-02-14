853baaf9a0c9df99de0dffdf706e33f65fb87c68
oleg.iakovenko <iakovenkoua@gmail.com>
2024-02-14 17:18:28 +0200

    add tests

--- src/test/java/com/breskul/bibernate/persistence/datasource/connectiopool/HikariCPTest.java
+++ src/test/java/com/breskul/bibernate/persistence/datasource/connectiopool/HikariCPTest.java
@@ -3,7 +3,6 @@ package com.breskul.bibernate.persistence.datasource.connectiopool;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
