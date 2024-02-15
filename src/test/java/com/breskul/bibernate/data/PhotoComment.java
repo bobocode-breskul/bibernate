123f0fab5ab5483025cf853cdb831397a4c5ab5d
RomanPryshchepa <roman.pryshchepa@gmail.com>
2024-02-15 19:25:27 +0100

    #75 fixed PhotoComment

--- src/test/java/com/breskul/bibernate/data/PhotoComment.java
+++ src/test/java/com/breskul/bibernate/data/PhotoComment.java
@@ -4,10 +4,7 @@ import com.breskul.bibernate.annotation.Column;
 import com.breskul.bibernate.annotation.DynamicUpdate;
 import com.breskul.bibernate.annotation.Entity;
 import com.breskul.bibernate.annotation.Id;
-import com.breskul.bibernate.annotation.JoinColumn;
-import com.breskul.bibernate.annotation.ManyToOne;
 import com.breskul.bibernate.annotation.Table;
-import com.breskul.bibernate.demo.entity.Photo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,7 +26,6 @@ public class PhotoComment {
   @Column(name = "text")
   private String text;
 
-  @ManyToOne
-  @JoinColumn(name = "photo_id", nullable = false)
-  private Photo photo;
+  @Column(name = "photo_id")
+  private int photo;
 }
