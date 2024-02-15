063960011badcd4ea372d7f0dcdc22893242f540
mfilimonov <filimonovkolay97@gmail.com>
2024-02-15 23:42:52 +0200

    add test

--- src/main/java/com/breskul/bibernate/persistence/datasource/BibernateDataSource.java
+++ src/main/java/com/breskul/bibernate/persistence/datasource/BibernateDataSource.java
@@ -10,14 +10,11 @@ import java.sql.SQLException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
-import javax.sql.DataSource;
-import lombok.Setter;
 
 /**
- * The BibernateDataSource class represents a data source for Bibernate connections. It provides flexibility to work with either
- * the default DriverManager-based mechanism for finding the appropriate Driver class or a specified custom Driver class.
+ * The BibernateDataSource class represents a data source for Bibernate connections. It provides flexibility to work with either the default
+ * DriverManager-based mechanism for finding the appropriate Driver class or a specified custom Driver class.
  */
-@Setter
 public class BibernateDataSource extends AbstractDataSource implements ConnectionPoolFactory {
 
   protected final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
@@ -28,16 +25,16 @@ public class BibernateDataSource extends AbstractDataSource implements Connectio
   private Driver delegateDriver;
 
   /**
-   * Default constructor for BibernateDataSource. This constructor is used when a specific Driver class is not specified.
-   * It relies on DriverManager to find the appropriate Driver class based on the connection URL.
+   * Default constructor for BibernateDataSource. This constructor is used when a specific Driver class is not specified. It relies on
+   * DriverManager to find the appropriate Driver class based on the connection URL.
    */
   public BibernateDataSource() {
   }
 
   /**
-   * Constructor for BibernateDataSource with a specified driver class. Use this constructor when you need to use a specific
-   * or custom Driver class. The provided driver class must be registered. DriverManager is used to find the Driver class by
-   * matching it with the given driver class name.
+   * Constructor for BibernateDataSource with a specified driver class. Use this constructor when you need to use a specific or custom
+   * Driver class. The provided driver class must be registered. DriverManager is used to find the Driver class by matching it with the
+   * given driver class name.
    *
    * @param properties The properties containing the connection URL, username, password, and driver class.
    * @throws BibernateException If the Driver with the specified name is not found or if there is an issue loading the class.
@@ -48,15 +45,7 @@ public class BibernateDataSource extends AbstractDataSource implements Connectio
     this.url = properties.url();
     this.username = properties.username();
     this.password = properties.password();
-    String driverClassName = Objects.requireNonNull(properties.driverClass(), "Driver class name cannot be null");
-    try {
-      Class<?> aClass = Class.forName(driverClassName);
-      Optional<Driver> driver = DriverManager.drivers().filter(aClass::isInstance).findFirst();
-      delegateDriver = driver.orElseThrow(() ->
-          new BibernateException("Driver with name %s not found".formatted(driverClassName)));
-    } catch (ClassNotFoundException e) {
-      throw new BibernateException("Error loading the Driver class: " + driverClassName, e);
-    }
+    this.delegateDriver = Objects.requireNonNull(prepareDriver(properties), "Driver class name cannot be null");
     return this;
   }
 
@@ -67,19 +56,31 @@ public class BibernateDataSource extends AbstractDataSource implements Connectio
 
   @Override
   public Connection getConnection(String username, String password) throws SQLException {
-    if (delegateDriver == null) {
-      findDriverByUrl(this.url);
-    }
     Properties properties = new Properties();
     properties.setProperty("user", username);
     properties.setProperty("password", password);
     return delegateDriver.connect(this.url, properties);
   }
 
-  private void findDriverByUrl(String url) {
+  private Driver prepareDriver(PersistenceProperties properties) {
+    String driverClassName = properties.driverClass();
+    if (driverClassName == null) {
+      return findDriverByUrl(this.url);
+    }
+    try {
+      Class<?> aClass = Class.forName(driverClassName);
+      Optional<Driver> driver = DriverManager.drivers().filter(aClass::isInstance).findFirst();
+      return driver.orElseThrow(() ->
+          new BibernateException("Driver with name %s not found".formatted(driverClassName)));
+    } catch (ClassNotFoundException e) {
+      throw new BibernateException("Error loading the Driver class: " + driverClassName, e);
+    }
+  }
+
+  private Driver findDriverByUrl(String url) {
     Objects.requireNonNull(url);
     try {
-      this.delegateDriver = DriverManager.getDriver(url);
+      return DriverManager.getDriver(url);
     } catch (SQLException e) {
       throw new BibernateException(String.format("Failed to get driver instance for jdbcUrl=%s", url), e);
     }
---
+++ src/test/java/com/breskul/bibernate/persistence/datasource/BibernateDataSourceTest.java
@@ -0,0 +1,72 @@
+package com.breskul.bibernate.persistence.datasource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.breskul.bibernate.persistence.datasource.connectionpools.C3P0;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+class BibernateDataSourceTest {
+
+  @Test
+  void given_persistenceProperties_when_propertiesValid_then_successfullyCreateDataSource() {
+    PersistenceProperties properties = new PersistenceProperties("jdbc:h2:file:~/tmp/test", "sa", "", null, null, null);
+
+    new BibernateDataSource().createDataSource(properties);
+
+    assertThatNoException();
+  }
+
+  @Test
+  void given_persistenceProperties_when_urlNotValid_then_successfullyCreateDataSource() {
+    PersistenceProperties properties = new PersistenceProperties("jdbc:myOwn:file:~/tmp/test", "sa", "", null, null, null);
+
+    assertThatThrownBy(() -> new BibernateDataSource().createDataSource(properties))
+        .hasMessage("Failed to get driver instance for jdbcUrl=jdbc:myOwn:file:~/tmp/test");
+  }
+
+  @Test
+  void given_persistenceProperties_when_withValidCustomDriverClass_then_successfullyCreateDataSource() {
+    PersistenceProperties properties = new PersistenceProperties("jdbc:h2:file:~/tmp/test", "sa", "", "org.h2.Driver", null, null);
+
+    new BibernateDataSource().createDataSource(properties);
+
+    assertThatNoException();
+  }
+
+  @Test
+  void given_persistenceProperties_when_withInvalidCustomDriverClass_then_successfullyCreateDataSource() {
+    PersistenceProperties properties = new PersistenceProperties("jdbc:h2:file:~/tmp/test", "sa", "", "org.h3.Driver", null, null);
+
+    assertThatThrownBy(() -> new BibernateDataSource().createDataSource(properties))
+        .hasMessage("Error loading the Driver class: org.h3.Driver");
+  }
+
+  @SneakyThrows
+  @Test
+  void given_persistenceProperties_when_propertiesValid_then_allParentMethodsWorkAsExpected() {
+    PersistenceProperties properties = new PersistenceProperties("jdbc:h2:file:~/tmp/test", "sa", "", null, null, null);
+
+    AbstractDataSource dataSource = new BibernateDataSource().createDataSource(properties);
+
+    assertThat(dataSource.getLoginTimeout()).isEqualTo(0);
+    assertThat(dataSource.isWrapperFor(BibernateDataSource.class)).isEqualTo(true);
+    assertThat(dataSource.isWrapperFor(C3P0.class)).isEqualTo(false);
+    assertThat(dataSource.unwrap(BibernateDataSource.class).getClass()).isEqualTo(BibernateDataSource.class);
+
+    assertThatThrownBy(() -> dataSource.setLoginTimeout(0))
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessage("setLoginTimeout");
+    assertThatThrownBy(dataSource::getLogWriter)
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessage("getLogWriter");
+    assertThatThrownBy(() -> dataSource.setLogWriter(null))
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessage("setLogWriter");
+    assertThatThrownBy(dataSource::getParentLogger)
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessage("getParentLogger");
+  }
+}
