43bc58eed8f0e6a3b487868562448d9c5ea7140e
Ruslan Hladchenko <60611618+hladchenko@users.noreply.github.com>
2024-02-16 22:37:15 +0200

    Add openSession javadoc (#104)

--- src/main/java/com/breskul/bibernate/persistence/SessionFactory.java
+++ src/main/java/com/breskul/bibernate/persistence/SessionFactory.java
@@ -16,6 +16,20 @@ public class SessionFactory {
     this.showSql = showSql;
   }
 
+  /**
+   * Opens a new session for interacting with the database. This session provides
+   * various methods for executing queries, managing transactions, and performing
+   * CRUD operations on entities.
+   * <p>
+   * The session is configured based on the provided {@code dataSource}, {@code dialect},
+   * and {@code showSql} settings. The {@code dialect} determines how SQL queries are
+   * generated specific to the database in use, and {@code showSql} configures whether
+   * SQL queries executed by the session should be logged.
+   *
+   * @return A new {@link Session} instance for database interaction.
+   * @throws SQLException If there is a problem obtaining a connection from the
+   *                      {@code dataSource} or initializing the session.
+   */
   public Session openSession() throws SQLException {
     return new Session(dataSource, dialect, showSql);
   }
